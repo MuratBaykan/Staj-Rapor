b161f296c7755f7dcaec4bb85cdc502515596e9f
MuratBaykan <109803931+MuratBaykan@users.noreply.github.com>
2022-08-03 15:41:57 +0300

    Add files via upload

---
+++ static/footer.php
@@ -0,0 +1,8 @@
+<footer>
+                    <div id="copyright">2022 | Tüm hakları saklıdır</div>
+                    <div id="socialfooter">
+                        <a href="#"><i class="fab fa-facebook-f social"></i></a>
+                        <a href="#"><i class="fab fa-google-plus-g social"></i></a>
+                        <a href="#"><i class="fab fa-twitter social"></i></a>
+                    </div>
+                </footer>
